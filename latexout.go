467bcab6990efb035e2d8a9ca37d2570339318fe
glen newton <glen.newton@gmail.com>
2014-06-18 12:31:30 -0400

    Added arraystretch to LaTeX output to improve text padding in table cells

--- latexout.go
+++ latexout.go
@@ -1,21 +1,20 @@
 package main
 
-import(
+import (
 	"fmt"
 	"strings"
 )
 
-type LaTeXOut struct{
-
+type LaTeXOut struct {
 }
 
-func (lo LaTeXOut) output(s []string, rpmInfo map[string] *RpmInfo, groupSet map[string]bool, nodes map[string]*Node) error{
+func (lo LaTeXOut) output(s []string, rpmInfo map[string]*RpmInfo, groupSet map[string]bool, nodes map[string]*Node) error {
 	fmt.Println("\\documentclass[11pt,landscape]{article}")
 	fmt.Println("")
 	fmt.Println("\\usepackage[landscape,paperwidth=10in,paperheight=8.5in]{geometry}")
 	fmt.Println("\\usepackage{longtable,microtype,savetrees}")
 	fmt.Println("\\usepackage[hyphens]{url}")
-	fmt.Println("\\usepackage{seqsplit}") 
+	fmt.Println("\\usepackage{seqsplit}")
 
 	fmt.Println("")
 	fmt.Println("\\oddsidemargin -.5cm")
@@ -34,6 +33,7 @@ func (lo LaTeXOut) output(s []string, rpmInfo map[string] *RpmInfo, groupSet map
 	//fmt.Println("\\tableofcontents")
 	//fmt.Println("\\newpage")
 	//fmt.Println("\\begin{landscape}")
+	fmt.Println("\\renewcommand*{\arraystretch}{1.4}")
 	fmt.Println("\\begin{longtable}{|p{2cm}|p{1.4cm}|p{4cm}|p{5cm}|p{4cm}|p{3cm}|}")
 	fmt.Println("\\hline")
 	fmt.Println("\\textbf{Name}& \\textbf{Version}& \\textbf{Summary}& \\textbf{Description}& \\textbf{URL}& \\textbf{Install Time}\\\\")
@@ -45,12 +45,12 @@ func (lo LaTeXOut) output(s []string, rpmInfo map[string] *RpmInfo, groupSet map
 	fmt.Println("\\endhead")
 
 	//fmt.Println("\\begin{enumerate}")
-	for r := range s {	
+	for r := range s {
 		//fmt.Println("\\section{" + escapeLatex(rpmInfo[s[r]].Name) + "}")
 		//fmt.Println("\\item{" + escapeLatex(rpmInfo[s[r]].Name) + "}")
 		//fmt.Println("\\begin{itemize}")
 		//for k,v := range rpmInfo[s[r]].Tags{
-		
+
 		//	v = strings.Replace(v, "\n", " ", -1)
 		//fmt.Println("\\item {\\bf" + escapeLatex("  " + k + ": ") + "}" + escapeLatex(v))
 		//fmt.Println("\\newline")
@@ -72,15 +72,15 @@ func (lo LaTeXOut) output(s []string, rpmInfo map[string] *RpmInfo, groupSet map
 	return nil
 }
 
-func escapeLatex(v string) string{
+func escapeLatex(v string) string {
 	v = strings.Replace(v, "\\", "\\textbackslash{}", -1)
 	v = strings.Replace(v, "_", "\\_", -1)
 	v = strings.Replace(v, "$", "\\$", -1)
 
-	v = strings.Replace(v, "#", "\\#", -1) 
+	v = strings.Replace(v, "#", "\\#", -1)
 	v = strings.Replace(v, "%", "\\%", -1)
 	v = strings.Replace(v, "^", "\\^{}", -1)
-	v = strings.Replace(v, "&", "\\&", -1) 
+	v = strings.Replace(v, "&", "\\&", -1)
 
 	v = strings.Replace(v, "{", "\\{", -1)
 
@@ -88,4 +88,4 @@ func escapeLatex(v string) string{
 	v = strings.Replace(v, "~", "\\~{}", -1)
 
 	return v
-	}
+}
